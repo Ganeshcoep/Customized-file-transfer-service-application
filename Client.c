7dc33784575ce58e6fd56a1b7743af4fa3f3f22a
Ganeshcoep <ganeshsalvi.coep@gmail.com>
2014-03-22 20:30:47 -0500

    Added files to Project
    Added files to Project

---
+++ Client.c
@@ -0,0 +1,328 @@
+/* ------------------------------------------------------------------------------
+Author: Ganesh S Salvi NetId=2021131759
+ACN HW1
+-------------------------------------------------------------------------------*/
+
+/*Header files*/
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>	
+
+
+//#define UDP_PORT 4050  //Port Number of Server
+//#define TCP_PORT 4051  //Port Number of Server
+#define BUFLEN 512    //Length of the data to be sent to the Server
+
+int main(int argc,char *argv[])
+{
+if(argc!=3)//strcmp(argv[0]," ") == 0||strcmp(argv[1]," ") == 0)
+{
+  printf("\nPlease pass valid command line parameters...\n");
+  return 1;
+}
+
+int udp_port=atoi(argv[1]);
+int tcp_port=atoi(argv[2]);
+
+
+printf("Client started.");
+
+struct sockaddr_in my_addr,server_addr;
+int sock_id,server_sock_id;
+char buf[BUFLEN],buf2[BUFLEN];
+socklen_t slen=sizeof(server_addr);
+
+/* setting the server details*/
+server_addr.sin_family=AF_INET;
+//server_addr.sin_port=htons(UDP_PORT); // short, network byte order
+server_addr.sin_port=htons(udp_port); // short, network byte order
+server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+if (server_addr.sin_addr.s_addr == INADDR_NONE ) {
+     printf("\nBad address."); }
+
+/* socket creation */
+sock_id=socket(PF_INET,SOCK_DGRAM,0);
+if(sock_id==-1){
+   err("\nsocket Creation Error\n");
+   return 1;
+}
+
+int filesize=0;
+while(1)
+{
+       printf("\nPlease enter command (Type 'quit' and press enter to quit) : ");
+       scanf("%[^\n]",buf);
+       getchar();
+       if(strcmp(buf,"quit") == 0)
+       break;
+       
+       sprintf(buf2,"%s",buf); //copy the command string before passing to tokenizer
+     
+       const char delimiter[2]=" ";
+       char *cmd,*filename,*no_chunks;
+       cmd=strtok(buf,delimiter);
+      if(strcmp(cmd,"lookup") == 0 || strcmp(cmd,"download") == 0)
+      {
+
+             if(strcmp(cmd,"lookup") == 0)
+	      {
+                      /* check if User has entered second parameter i.e. filename*/
+                      filename=strtok(NULL,delimiter);
+		      if(filename==NULL){
+		      printf("\nThe filename is missing");
+		      continue;
+                      }
+      
+		      /* send lookup query to server*/
+                       int sentbytes=sendto(sock_id, buf2, BUFLEN, 0, (struct sockaddr*)&server_addr, slen);
+		       if (sentbytes==-1)
+		           err("\nServer lookup sendtoerror()");
+		       if (sentbytes==0)
+		           err("\nServer is not listening");
+		      
+//   		      int size=0;
+		      char bytesMsg[50];
+		      if (recvfrom(sock_id, bytesMsg, sizeof(bytesMsg), 0, (struct sockaddr*)&server_addr, &slen)==-1){
+		      err("\n Server lookup recvfromerror()");
+                      continue;
+		      }
+                      filesize=atoi(bytesMsg);
+                      printf("\n%d",filesize);
+		           				
+             }
+            if(strcmp(cmd,"download") == 0)
+             {
+
+                      /* check if User has entered second and parameter i.e. filename and no_of_chunks*/
+                      filename=strtok(NULL,delimiter);
+		      if(filename==NULL){
+		      printf("\nThe filename is missing");
+		      continue;
+		      }
+                      no_chunks=strtok(NULL,delimiter);
+		      if(no_chunks==NULL){
+		      printf("\nPlease enter no. of chunks...");
+		      continue;
+                      }
+             
+	       /* send the download command to the server*/
+               int sentbytes=sendto(sock_id, buf2, BUFLEN, 0, (struct sockaddr*)&server_addr, slen);
+		       if (sentbytes==-1)
+		          { err("sendtoerror()");continue;}
+		       if (sentbytes==0)
+		          { err("Server is not listening");continue;}
+	            
+               //int filesize=78;
+               int no_of_chunks=atoi(no_chunks);
+               printf("\nDownloading started...");
+               int success=download(filename,filesize,no_of_chunks,tcp_port);
+               if(success==1)
+	               {
+                       printf("\nDownload Successful.");
+	               }
+               else
+		       {
+                        printf("\nDownload Failed.");
+		       }
+            }
+      }
+     else 
+     {
+      printf("\nUnknown Command");
+      printf("\nPlease use below commnads: \nlookup[SPACE][Filename] OR \ndownload[SPACE][Filename][SPACE][No of chunks] \nNote:Both the commands are case sensitive)");
+      continue;
+     } 
+  
+}
+}
+
+int download(char *filename,int filesize,int no_of_chunks,int tcp_port)
+{
+
+/* Create sockets for TCP connections*/
+/*-------------------------------- */
+struct sockaddr_in my_addr,server_addr;
+int sock_id[no_of_chunks],server_sock_id;
+char buf[BUFLEN],buf2[BUFLEN];
+socklen_t slen=sizeof(server_addr);
+
+//setting the server details
+server_addr.sin_family=AF_INET;
+//server_addr.sin_port=htons(TCP_PORT); 
+server_addr.sin_port=htons(tcp_port);
+server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+if (server_addr.sin_addr.s_addr == INADDR_NONE ) {
+     printf("\nBad address."); }
+
+/* socket creation for K chunks*/
+int i;
+for (i=0;i<no_of_chunks;i++)
+{
+	sock_id[i]=socket(PF_INET,SOCK_STREAM,0);
+	if(sock_id[i]==-1){
+	   err("\nsocket Creation Error\n");
+   	return 0;
+	}
+}
+
+/* Connect to the Server*/
+for (i=0;i<no_of_chunks;i++)
+{
+  if (connect(sock_id[i],(struct sockaddr*)&server_addr, slen) < 0) 
+	{
+	      error("\nERROR connecting");
+	   	return 0;
+	}
+}
+
+/*Divide the requests into K parts*/
+int cntt=0;
+int K=no_of_chunks;
+int each_req_size=filesize/K;
+int start_index=0,end_index=0;
+int cnt=0;
+int No_of_bytes[no_of_chunks-1];
+
+while(end_index<filesize-1)
+{
+	if(cnt!=0) 
+	start_index=end_index+1;
+
+	end_index=start_index+each_req_size-1;
+
+	//last chunk can have more data or less data e.g if Filesize is 78 and no of chunks is 5 then 5th chunk will have 18 bytes and if n		o of chunks is 4 then 4th chunk will have 18 bytes.
+	if(cnt==K-1)
+	end_index=filesize-1;
+        char buff[BUFLEN]="abc\0";
+	sprintf(buff, "%s %d %d ",filename, start_index, end_index);
+//	printf("\nBuffer to be sent: %s size id :%d",buff,strlen(buff));
+	No_of_bytes[cnt]=end_index-start_index+1;
+
+	/* Send the Request for file transfer*/
+
+	int m=send(sock_id[cnt], buff, strlen(buff),0);
+	if(m==-1)
+        {
+	printf("\n error in send");
+        return 0;
+        }
+        if(m!=strlen(buff))
+        {
+	printf("\n error in send");
+        return 0;
+        }
+	cnt++;
+	printf("\nChunk request %d sent %s %d %d",cnt,filename,start_index,end_index);
+}
+
+int rv,n;
+fd_set readfds;
+struct timeval tv;
+char *filedata[no_of_chunks-1];
+
+int a=0;
+for(a=0;a<no_of_chunks;a++)
+filedata[a]=(char*)malloc(sizeof(char)*No_of_bytes[a]); 
+
+// the n param in select()
+n = sock_id[no_of_chunks-1] + 1;
+//printf("\nlast sock id is %d and N is %d",sock_id[no_of_chunks-1],n);
+
+// wait until either socket has data ready to be recv()d (timeout 10.5 secs)
+
+int count=0;
+while(count<no_of_chunks)
+{
+
+tv.tv_sec = 10;
+tv.tv_usec = 500000;
+FD_ZERO(&readfds);
+
+// add descriptors to the set
+for(a=0;a<no_of_chunks;a++)
+{
+FD_SET(sock_id[a], &readfds);
+
+}
+
+
+rv = select(n, &readfds, NULL, NULL, &tv);
+if (rv == -1)
+{
+    perror("\nselect"); // error occurred in select()
+}
+else if (rv == 0) 
+{
+    printf("\nTimeout occurred!  No data after 10.5 seconds.\n");
+}
+else
+{
+	// one or both of the descriptors have data
+	for(a=0;a<no_of_chunks;a++)
+	{
+		 if (FD_ISSET(sock_id[a], &readfds))
+		 {
+	                //printf("\nData Received from socket %d  and count is",sock_id[a]);
+	           
+			 int rcvcnt=receiveAllData(sock_id[a],filedata[a],No_of_bytes[a]);
+			// int rcvcnt=recv(sock_id[j], filedata[j],No_of_bytes[j], 0);
+		   	 if(rcvcnt==-1)
+		   	 {
+	   		  printf("\n receiveALlerror");
+		   	  return 0;
+			 }
+	                 count++;
+		         //printf("\nData Received from Server : %s cnt is %d and Received all? 0-success %d",filedata[a],count,rcvcnt);
+	           
+	
+	         }
+	}//end of for
+}//end of else
+}//end of while
+
+/* Write all the data recieved to the file*/
+ FILE *fp;
+ fp = fopen(filename, "wb");
+ 
+	 for(a=0;a<no_of_chunks;a++)
+	{
+         
+         if(a==0)
+         fseek(fp, 0, SEEK_SET);	 
+         else
+         fseek(fp, 0, SEEK_END);	 
+
+	 fwrite(filedata[a], sizeof(char),No_of_bytes[a], fp); 
+	}
+ fclose(fp);
+
+
+/* Close the sockets*/
+for (i=0;i<no_of_chunks;i++)
+{
+  close(sock_id[i]);
+}
+
+return 1;
+}
+
+int receiveAllData(int sockid, char *buf, int No_Of_Bytes)
+{
+    int totalreceived = 0; // how many bytes we've received
+    int bytesleft = No_Of_Bytes; // how many we have left to receive
+    int n;
+
+    while(totalreceived < No_Of_Bytes) {
+        n = recv(sockid, buf+totalreceived, bytesleft, 0);
+        if (n == -1) { break; }
+        totalreceived += n;
+        bytesleft -= n;
+    }
+
+    No_Of_Bytes = totalreceived; // return number actually received here
+
+     return n==-1?-1:0; // return -1 on failure, 0 on success
+}
---
+++ Server.c
@@ -0,0 +1,295 @@
+/* ------------------------------------------------------------------------------
+Author: Ganesh S Salvi NetId=2021131759
+ACN HW1
+-------------------------------------------------------------------------------*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netdb.h> // For INADDR_ANY and sockaddr_in
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <stdlib.h>	//for itoa call
+#include <pthread.h>	
+//#define UDP_PORT 4050
+//#define TCP_PORT 4051
+#define BUFLEN 512
+#define BACKLOG 100     // how many pending connections queue will hold used for listen
+
+int sendAll(int s, char *buf, int *len);
+void *download(void *new_sock_id);
+
+int main(int argc,char *argv[])
+{
+
+if(argc!=3)//strcmp(argv[0]," ") == 0||strcmp(argv[1]," ") == 0)
+{
+  printf("\nPlease pass valid command line parameters...\n");
+  return 1;
+}
+
+int udp_port=atoi(argv[1]);
+int tcp_port=atoi(argv[2]);
+
+/*Get the details of the Server*/
+struct hostent *server;
+server = gethostbyname("localhost");
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        return 0;//exit(0);
+    }
+
+printf("Server is running on UDP port %d TCP port %d.\n",udp_port,tcp_port);
+
+
+/* Setup Server for UDP connection*/
+
+struct sockaddr_in my_addr,client_addr;
+int sock_id,client_sock_id;
+char buf[BUFLEN];
+socklen_t slen=sizeof(client_addr);
+
+my_addr.sin_family=AF_INET;
+//my_addr.sin_port=htons(UDP_PORT); // short, network byte order
+my_addr.sin_port=htons(udp_port); // short, network byte order
+my_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+
+/* Create socket for UDP */
+sock_id=socket(PF_INET,SOCK_DGRAM,0);
+if(sock_id==-1){
+   err("\nsocket Error");
+   return 1;
+}
+
+/*Bind socket for UDP*/
+int bind_status=bind(sock_id,(struct sockaddr*)&my_addr,sizeof(my_addr));
+if(bind_status==-1){
+   err("\nsocket bind Error");
+   return 1;
+}
+
+/* Setup Server for TCP connection*/
+struct sockaddr_in tcp_my_addr,tcp_client_addr;
+int tcp_sock_id,tcp_client_sock_id;
+char tcp_buf[BUFLEN];
+socklen_t tcp_slen=sizeof(tcp_client_addr);
+tcp_my_addr.sin_family=AF_INET;
+//tcp_my_addr.sin_port=htons(TCP_PORT); // short, network byte order
+tcp_my_addr.sin_port=htons(tcp_port); // short, network byte order
+tcp_my_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+
+/* Create socket for TCP */
+tcp_sock_id=socket(PF_INET,SOCK_STREAM,0);
+if(tcp_sock_id==-1){
+   err("\nsocket Error");
+   return 1;
+}
+
+/*Bind socket for TCP*/
+int tcp_bind_status=bind(tcp_sock_id,(struct sockaddr*)&tcp_my_addr,sizeof(tcp_my_addr));
+if(tcp_bind_status==-1){
+   err("\nsocket bind Error");
+   return 1;
+}
+
+if(listen(tcp_sock_id,10)==-1){
+printf("\nlistenerror");
+return 1;
+}
+
+while(1)
+{
+      if (recvfrom(sock_id, buf, BUFLEN, 0, (struct sockaddr*)&client_addr, &slen)==-1)
+         err("\nrecvfrom()");
+     
+      const char delimiter[2]=" ";
+      char *cmd,*filename;
+      cmd=strtok(buf,delimiter);
+
+      if(strcmp(cmd,"lookup") == 0)
+      {
+      
+      filename=strtok(NULL,delimiter);
+      
+      char *cwd="./Data";
+      struct stat buff;
+      DIR *dir;
+      struct dirent *ent;
+      int flag=0;
+      long size=0;
+      if ((dir = opendir (cwd)) != NULL) {
+
+       	 char bytesMsg[256];  //used to send reply to client
+         while ((ent = readdir (dir)) != NULL) 
+         {
+         	if(strcmp(filename,ent->d_name) == 0)
+         	{
+		 flag=1; // File found
+	         stat (ent->d_name,&buff);
+                 size = (long)buff.st_size;
+	         printf("\nUDP look-up query received for file %s. Returning query response :%d",filename,size);
+	         sprintf(bytesMsg, "%ld", size);
+	         //printf("%s\n", bytesMsg);
+	         if (sendto(sock_id, bytesMsg, sizeof(bytesMsg), 0, (struct sockaddr*)&client_addr, slen)==-1)
+	         err("\nsendto()");
+        	 break; // File found and response sent to client so break the loop
+        	 }
+
+         }//while
+         if(flag==0) //File not found
+	 { 
+         	printf("\nUDP look-up query received for file %s.",filename);
+         	printf("Returning query response :%d",size);
+		sprintf(bytesMsg, "%ld", size);
+//         	sprintf(bytesMsg, "%s","FNF");
+	       	if (sendto(sock_id, bytesMsg, sizeof(bytesMsg), 0, (struct sockaddr*)&client_addr, slen)==-1)
+         	err("\nsendto()");
+       
+         }
+         
+
+         closedir (dir);
+         } else {
+          //could not open directory 
+          perror ("");
+         }
+      }else {
+      		if(strcmp(cmd,"download") == 0)
+		      {
+                       char *filename=strtok(NULL,delimiter);
+                       char *no_chunks=strtok(NULL,delimiter);
+                       long no_of_chunks=atoi(no_chunks);
+
+                       //Call lookup first if lookup is successful then start download
+                       //printf("%s size %ld",filename,no_of_chunks);
+                       struct sockaddr_storage client_addrs[no_of_chunks];
+                       int new_sock_id[no_of_chunks];
+                       int i;
+                       int flag=1;
+                       for (i=0;i<no_of_chunks;i++)
+                       {
+                       socklen_t addr_size=sizeof(client_addrs[i]);
+                       new_sock_id[i] = accept(tcp_sock_id,(struct sockaddr*)&client_addrs[i],&addr_size);
+                       //printf("\n Sock Id: %d",new_sock_id[i]);
+                       if(new_sock_id[i]==-1)
+                       flag=0;
+                       }
+                       printf("\nTCP connection received.");
+                       if(flag==1)
+                       {
+                       		pthread_t thread[no_of_chunks];
+	                       int rc[no_of_chunks];
+        	               for (i=0;i<no_of_chunks;i++)
+        	               {
+        	               rc[i] = pthread_create(&thread[i], NULL, download,(void *) &new_sock_id[i]);
+        	               pthread_join(thread[i],NULL);
+        	               }
+               		       printf("\nFile sent to the client Successfully...");       
+		      }
+                     }
+		else 
+		     { 
+                      printf("\nUnknown Command");
+		      printf("\nPlease use commnads: lookup or download (both are case sensitive)");
+                     }
+                      
+      }
+printf("\n");
+}//while loop
+ 
+close(sock_id);
+close(tcp_sock_id);
+
+pthread_exit(NULL);
+
+return 0;
+}
+
+
+int sendAll(int s, char *buf, int *len)
+{
+    int total = 0;        // how many bytes we've sent
+    int bytesleft = *len; // how many we have left to send
+    int n=0;
+//    printf("\nThe socket id from SendALL is %d %s %d",s,buf,*len);
+
+    while(total < *len) {
+        n = send(s, buf+total, bytesleft, 0);
+  //  printf("\nThe no. of bytes sent from SendALL is %d ",n);
+        if (n == -1) { break; }
+        total += n;
+        bytesleft -= n;
+    }
+
+    *len = total; // return number actually sent here
+
+    return n==-1?-1:0; // return -1 on failure, 0 on success
+} 
+
+void *download(void *new_sock_id)
+{
+int *sock_id=(int *) new_sock_id;
+
+//printf("\nThread STARTED");
+
+int i;
+char buf[2000];
+FILE *fp;
+char *buff;
+
+//printf("\nSock id: %d", *sock_id);
+
+bzero(buf,2000);
+if(recv(*sock_id, buf, sizeof(buf),0)==-1)
+printf("\n buferror");
+
+//printf("\nReceived : %s",buf);
+
+
+//Parse the request 
+const char delimiter[2]=" ";
+      char *filename;
+      int start_index=0,end_index=0;
+      filename=strtok(buf,delimiter);
+      char *start=strtok(NULL,delimiter);
+      char *end=strtok(NULL,delimiter);
+      start_index=atoi(start);
+      end_index=atoi(end);
+      int No_of_bytes=end_index-start_index+1;
+//printf("\nFilename:%s start_index:%d end_index:%dNo_of_bytes:%d",filename,start_index,end_index,No_of_bytes);
+printf("\nChunk request of %d bytes in ""%s"" at offset %d.",No_of_bytes,filename,start_index);
+
+char filepath[1000];
+char *folderpath="./Data/";
+sprintf(filepath, "%s%s",folderpath,filename);
+
+fp = fopen(filepath, "rb");
+if (fp == NULL) 
+{
+    printf("\nFile not found!\n");
+    return 0;
+}
+else
+{
+	buff=(char*)malloc(sizeof(char)*No_of_bytes); 
+	fseek(fp, start_index-1, SEEK_SET);
+        bzero(buff,No_of_bytes);
+	fread(buff, sizeof(char), No_of_bytes, fp); 
+//      printf("\nBuffer read from file: %s",buff);
+        buff[No_of_bytes]='\0';
+        sleep(2);
+	int m=sendAll(*sock_id, buff,&No_of_bytes);
+	if(m==-1)
+	printf("\n error");
+	fclose(fp);
+}
+
+free(buff);
+//printf("\nThread finished : %s",buf);
+//printf("\n");
+pthread_exit(NULL);
+}
+
+
+
